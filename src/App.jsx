c231e5d017ae5c29b6a80b66e975780fbc56a121
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-05-02 23:59:08 -0500

    Created Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 import Home from "./pages/Home";
-
 import About from "./pages/About";
-
 import SignIn from "./pages/SignIn";
-
 import SignUp from "./pages/SignUp";
-
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
+
 
 function App() {
   return (
+    
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+function Header(){
+
+  return (
+    
+    <header>
+      
+      <h1>
+        <span>Urban</span>
+        <span>UTOPIA</span>
+      </h1>
+
+    </header>
+
+  );
+}
+
+export default Header;
