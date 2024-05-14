0a1580349c0c2c55041e737eea0e7cb6a9aa8a1f
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-05-14 16:10:39 -0500

    Updated the mongo_uri env variable

--- back-end-api/index.js
+++ back-end-api/index.js
@@ -2,15 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config(); //loading the env variables
 
 // const express = require("express");
 //in order to use the import statement and not the require keyword of common JS, update the package.json file in root, add property "type":module
 
 const app = express();
 
-mongoose.connect(process.env.MONGO)
-  .then(()=> console.log("Connected to the DB"))
+mongoose.connect(process.env.MONGO_URI)
+  .then(()=> console.log("Connected to the DB")) //to check if the db connection is successful or not.
   .catch((err)=> console.log(err));
 
 app.listen(3000, ()=>{
