3ed6cd895b8dd330b25411d57cc912e50355e18b
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-05-18 22:33:49 -0500

    Template created for the sign-up page

--- front-end-client/src/pages/SignUp.jsx
+++ front-end-client/src/pages/SignUp.jsx
@@ -1,7 +1,14 @@
 function SignUp(){
   return(
     <div>
-      Sign-Up
+      <h1 className="text-center my-7 font-semibold text-3xl">Sign Up</h1>
+
+      <form className="flex ">
+        <input type="text" placeholder="username" className="border p-3 rounded-lg" id="username" /> {/*"id" attribute to know which input is changing so that we can manipulate it*/}
+        <input type="text" placeholder="password" className="border p-3 rounded-lg" id="password"/>
+        <input type="text" placeholder="email" className="border p-3 rounded-lg" id="email"/>
+      </form>
+
     </div>
   );
 }
