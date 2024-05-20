d359d3d369f53748413cd632e518c51603f5062a
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-05-20 17:51:07 -0500

    Using useState hook to record the sign-up form data

--- front-end-client/src/pages/SignUp.jsx
+++ front-end-client/src/pages/SignUp.jsx
@@ -1,14 +1,28 @@
 import {Link} from "react-router-dom";
 
+import { useState } from "react";
+
 function SignUp(){
+
+  const [formData, setFormData] = useState({}); // used the "useState" hook for recording/managing the sign-up from data.
+  const [previousFormData, setPreviousFormData] = useState({});
+
+  const handleSignUpChange = (e) =>{
+      setFormData({
+        ...formData, // spreading the formData object 
+        [e.target.id] : e.target.value
+      });
+  }
+  console.log(formData);
+
   return(
     <div className="p-3 max-w-lg mx-auto"> {/*mx-auto to bring the items in the center. max-w-lg to ensure the width of the field do not exceed the lg/large viewport*/}
       <h1 className="text-center my-7 font-semibold text-3xl">Sign Up</h1>
 
       <form className="flex flex-col gap-4"> {/* gap for the spacing between them*/}
-        <input type="text" placeholder="Username" className="border p-3 rounded-lg" id="username" /> {/*"id" attribute to know which input is changing so that we can manipulate it*/}
-        <input type="text" placeholder="Password" className="border p-3 rounded-lg" id="password"/>
-        <input type="text" placeholder="Email" className="border p-3 rounded-lg" id="email"/>
+        <input type="text" placeholder="Username" className="border p-3 rounded-lg" id="username" onChange={handleSignUpChange}/> {/*"id" attribute to know which input is changing so that we can manipulate it*/}
+        <input type="password" placeholder="Password" className="border p-3 rounded-lg" id="password" onChange={handleSignUpChange}/>
+        <input type="email" placeholder="Email" className="border p-3 rounded-lg" id="email" onChange={handleSignUpChange}/>
 
         <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 transition disabled:opacity-80">Sign up</button>
 
@@ -17,7 +31,7 @@ function SignUp(){
       <div className="flex gap-2 mt-5"> {/*mt- for the margin top*/}
         <p>Have an account? </p>
         <Link to={"/sign-in"}>
-          <span className="text-blue-700">Sign in</span>
+          <span className="text-blue-700 hover:text-blue-500">Sign in</span>
         </Link>
       </div>
 
