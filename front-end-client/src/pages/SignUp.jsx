c7c04be1299538056ecbf3baf8871a3e33b4eff4
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-05-22 14:00:24 -0500

    Installed axios dependency for making HTTP requests from the client

--- front-end-client/src/pages/SignUp.jsx
+++ front-end-client/src/pages/SignUp.jsx
@@ -5,29 +5,35 @@ import { useState } from "react";
 function SignUp(){
 
   const [formData, setFormData] = useState({}); // used the "useState" hook for recording/managing the sign-up from data.
+  // const [previousState, setPreviousState] = useState({}); // for troubleshooting the states
 
   const handleSignUpChange = (e) =>{
+
+      // setPreviousState(formData);
       setFormData({
-        ...formData, // spreading the formData object to preserve the previous state.
-        [e.target.id] : e.target.value // updating just the required/modified/changed fields inside the formData. We're always recreating a new version of formData and discarding the previous one.
+        ...formData, // spreading the formData object to copy the data from the previous state obj to the new obj.
+        [e.target.id] : e.target.value // updating just the required/modified/changed fields inside the formData. We're always recreating a new version of formData object.
       });
   }
-  console.log(formData);
-
+  
   const handleSignUpSubmit= (e) => {
+    e.preventDefault(); //to ensure page does not referesh on submitting the form.
+    console.log(formData);
+    console.log("Submitted");
     
+
   }
 
   return(
     <div className="p-3 max-w-lg mx-auto"> {/*mx-auto to bring the items in the center. max-w-lg to ensure the width of the field do not exceed the lg/large viewport*/}
       <h1 className="text-center my-7 font-semibold text-3xl">Sign Up</h1>
 
-      <form className="flex flex-col gap-4"> {/* gap for the spacing between them*/}
+      <form className="flex flex-col gap-4" onSubmit={handleSignUpSubmit}> {/* gap for the spacing between them*/}
         <input type="text" placeholder="Username" className="border p-3 rounded-lg" id="username" onChange={handleSignUpChange}/> {/*"id" attribute to know which input is changing so that we can manipulate it*/}
         <input type="password" placeholder="Password" className="border p-3 rounded-lg" id="password" onChange={handleSignUpChange}/>
         <input type="email" placeholder="Email" className="border p-3 rounded-lg" id="email" onChange={handleSignUpChange}/>
 
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 transition disabled:opacity-80" onSubmit={handleSignUpSubmit}>Sign up</button>
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 transition disabled:opacity-80">Sign up</button>
 
       </form>
 
