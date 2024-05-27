57bd71b95d17b56b51a2f52b3dc8c26bc9a524e1
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-05-26 21:51:26 -0500

    Added styles to the Sign In page

--- front-end-client/src/pages/SignIn.jsx
+++ front-end-client/src/pages/SignIn.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function SignIn(){
   return(
     <div className="max-w-lg mx-auto">
@@ -7,11 +9,14 @@ function SignIn(){
       </h1>
 
       <form className="flex flex-col gap-6">
-        <input type="text" placeholder="Username" className="p-3 rounded-lg border"/>
-        <input type="password" placeholder="Password" className="p-3 rounded-lg border"/>
-        <button>Sign In</button>
+        <input type="text" placeholder="Username" className="p-3 rounded-lg border" id="username"/>
+        <input type="password" placeholder="Password" className="p-3 rounded-lg border" id="password"/>
+        <button className="bg-slate-700 text-white p-3 rounded-lg">SIGN IN</button>
       </form>
-
+      <div className="mt-4 flex gap-2">
+        <h1>Have an account?</h1>
+        <Link to={"/sign-in"} className="text-blue-700">Sign In</Link>
+      </div>
     </div>
   );
 }
