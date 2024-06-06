70228c22cc692d69fad781666500a4eae97166b7
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-06-05 23:56:53 -0500

    Created slice for the redux logic

--- front-end-client/src/redux/store.js
+++ front-end-client/src/redux/store.js
@@ -2,4 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
   reducer:{},
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: false,
+  }), //to ensure we don't get the error
 });
