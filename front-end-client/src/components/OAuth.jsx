92801a0d466646cd76de7238c809d112e5162ce4
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-07-05 15:27:05 -0500

    Initialized firebase using its SDK

--- front-end-client/src/components/OAuth.jsx
+++ front-end-client/src/components/OAuth.jsx
@@ -1,7 +1,18 @@
 function OAuth(){
 
+  const handleGoogleClick = () => {
+
+    try{
+
+    }
+    catch(err) {
+      console.log(err);
+    }
+
+  }
+
   return (
-    <button type="button" className="bg-red-700 text-white hover:opacity-90 p-3 rounded-lg transition uppercase">
+    <button type="button" onClick = {handleGoogleClick} className="bg-red-700 text-white hover:opacity-90 p-3 rounded-lg transition uppercase">
       Continue with Google
     </button>
   )
---
+++ front-end-client/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDtF4MoFoG5tybWD4Zp1hYR9X5-ZhBBPSs",
+  authDomain: "mern-react-app-417f4.firebaseapp.com",
+  projectId: "mern-react-app-417f4",
+  storageBucket: "mern-react-app-417f4.appspot.com",
+  messagingSenderId: "554857891516",
+  appId: "1:554857891516:web:af6c5b527836ece6ff32b9",
+  measurementId: "G-2HW2Z8DYV3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
