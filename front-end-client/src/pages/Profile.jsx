6b21c2c5251aebd319009dfbdaaf27dbe07fdfde
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-07-16 19:00:18 -0500

    feature: Upload/Change profile picture

--- front-end-client/src/pages/Profile.jsx
+++ front-end-client/src/pages/Profile.jsx
@@ -1,17 +1,21 @@
 import { useSelector } from "react-redux";
-
+import {useRef} from "react";
 
 function Profile(){
 
   const {currentUser} = useSelector(state => state.user);
+
+  const fileRef = useRef(null); //using the "useRef" hook to provide reference of the image input to the profile picture so that
+  //when a user clicks on the profile pic they're prompted to change the profile image
 
   console.log(currentUser);
 
   return(
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="font-semibold text-3xl my-8 text-center">Profile</h1>
+      <input type="file" ref={fileRef} accept="image/*" hidden/> {/*Image file input kept hidden and "accept*" property ensuring only image files are accepted */}
       <form className="flex flex-col gap-4">
-      <img src= {currentUser.avatar} alt="profile-picture" className="h-24 object-cover w-24 rounded-full self-center hover:cursor-pointer"/>
+      <img onClick={()=> fileRef.current.click()} src= {currentUser.avatar} alt="profile-picture" className="h-24 object-cover w-24 rounded-full self-center hover:cursor-pointer"/>
       <input type="text" id="username" value={currentUser.username} className="border p-3 rounded-lg outline-slate-400"/>
       <input type="text" id="email" value={currentUser.email} className="border p-3 rounded-lg outline-slate-400"/>
       <input type="password" id="password" placeholder="Password" className="border p-3 rounded-lg outline-slate-400"/>
