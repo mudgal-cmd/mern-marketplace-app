c0ad751430085976e0374261a56332153d6a142d
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-07-18 23:38:58 -0500

    Saved the updated  profile piture name to the state

--- front-end-client/src/pages/Profile.jsx
+++ front-end-client/src/pages/Profile.jsx
@@ -5,17 +5,22 @@ function Profile(){
 
   const {currentUser} = useSelector(state => state.user);
 
-  const [file, setFile] = useState(null);
+  const [file, setFile] = useState(undefined);
 
   const fileRef = useRef(null); //using the "useRef" hook to provide reference of the image input to the profile picture so that
   //when a user clicks on the profile pic they're prompted to change the profile image
 
   console.log(currentUser);
 
+  const getFile = (e) => {
+    console.log(e.target.files[0]);
+    setFile(e.target.files[0].name);
+  } // function to get the name of the current profile picture.
+
   return(
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="font-semibold text-3xl my-8 text-center">Profile</h1>
-      <input type="file" ref={fileRef} accept="image/*" hidden/> {/*Image file input kept hidden and "accept*" property ensuring only image files are accepted */}
+      <input type="file" onChange={(e) => getFile(e)} ref={fileRef} accept="image/*" hidden/> {/*Image file input kept hidden and "accept*" property ensuring only image files are accepted */}
       <form className="flex flex-col gap-4">
       <img onClick={()=> fileRef.current.click()} src= {currentUser.avatar} alt="profile-picture" className="h-24 object-cover w-24 rounded-full self-center hover:cursor-pointer"/>
       <input type="text" id="username" value={currentUser.username} className="border p-3 rounded-lg outline-slate-400"/>
