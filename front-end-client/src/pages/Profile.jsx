e669befb0d432df24eaea7d163b06a678a19060f
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-07-24 22:10:00 -0500

    Completed the image upload functionality + image file check during upload

--- front-end-client/src/pages/Profile.jsx
+++ front-end-client/src/pages/Profile.jsx
@@ -27,8 +27,13 @@ function Profile(){
   const getFile = (e) => {
     // console.log(e.target.files[0]);
     // console.log(e.target.files[0].name);
-    setFile(e.target.files[0]);
-    console.log(file);
+
+    const targetFile = e.target.files[0];
+
+    targetFile.type.startsWith("image")? setFile(targetFile): setFileUploadError("Incorrect image file format"); //allowing only image type files to upload/store in firebase
+
+    // setFile(e.target.files[0]);
+    console.log(file);// will get undefined because state update happens async because the state hasn't processed yet and we're already printing it.
   } // function to get the name of the current profile picture.
 
 
@@ -39,7 +44,7 @@ function Profile(){
     }
 
   }, [file]);// we want to see the updated profile image, so useEffect will force a re-render everytime the value of file changes.
-
+  console.log(file);
   const handleFileUpload =(file) => {
 
     const storage = getStorage(app); // initialized the mern firebase app/project, for the firebase to identify that it's the same app.
@@ -52,7 +57,7 @@ function Profile(){
     const uploadFileTask = uploadBytesResumable(storageRef, file);
 
     uploadFileTask.on('state_changed', //'state_changed' will track the changes and give us the snapshot.
-    (snapshot) => {
+    (snapshot) => { // tracking the changes of the image/versions uploaded
         const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes)*100);
 
         // console.log("Upload is ",progress,"%done");
@@ -60,12 +65,13 @@ function Profile(){
         // console.log(fileUploadPerc);
       }
     ,
-    (error) => {setFileUploadError(true);},
+    (error) => {setFileUploadError(error);},
     ()=>{
       getDownloadURL(uploadFileTask.snapshot.ref).then(
         (downloadURL)=> {
           setUpdateFormData({...updateFormData, avatar: downloadURL});
           console.log(downloadURL);
+
         } //downloadURL is the result of the getDownloadURL promise function getting resolved.
       );
     });
@@ -77,8 +83,9 @@ function Profile(){
   
   }
 
-  const handleFormData = ()=>{
-
+  const handleFormData = (e)=>{
+    console.log(e);
+    // e.target.readOnly = true;
   }
 
 
@@ -87,8 +94,22 @@ function Profile(){
       <h1 className="font-semibold text-3xl my-8 text-center">Profile</h1>
       <input type="file" onChange={(e) => getFile(e)} ref={fileRef} accept="image/*" hidden/> {/*Image file input kept hidden and "accept*" property ensuring only image files are accepted */}
       <form className="flex flex-col gap-4">
-      <img onClick={()=> fileRef.current.click()} src= {currentUser.avatar} alt="profile-picture" className="h-24 object-cover w-24 rounded-full self-center hover:cursor-pointer"/>
-      <input type="text" id="username" value={currentUser.username} className="border p-3 rounded-lg outline-slate-400" onFocus={handleFor}/>
+      <img onClick={()=> fileRef.current.click()} src= {updateFormData.avatar || currentUser.avatar} alt="profile-picture" className="h-24 object-cover w-24 rounded-full self-center hover:cursor-pointer"/> {/* if the formdata has avatar, it will be shown else the image in db will be displayed*/}
+      <p className="self-center text-sm">
+        {
+          fileUploadError? 
+          <span className="text-red-700">
+            {fileUploadError}
+          </span>
+          : 
+          fileUploadPerc>0 && fileUploadPerc<100? 
+          <span className="text-blue-700">{`Uploaded ${fileUploadPerc} %`}</span>
+          : fileUploadPerc === 100?
+          <span className="text-green-700">Image Uploaded Successfully</span>
+          :""
+        }
+      </p>
+      <input type="text" id="username" value={currentUser.username} className="border p-3 rounded-lg outline-slate-400" onFocus={(e)=>handleFormData(e)}/>
       <input type="text" id="email" value={currentUser.email} className="border p-3 rounded-lg outline-slate-400"/>
       <input type="password" id="password" placeholder="Password" className="border p-3 rounded-lg outline-slate-400"/>
       <button className="bg-slate-700 text-white rounded-lg p-3 hover:opacity-90 disabled:opacity-80 transition">Update Profile</button>
