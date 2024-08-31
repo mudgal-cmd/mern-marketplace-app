5b668f3785d08a99999adb84e271b68732c33706
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-08-31 01:12:22 -0500

    Designed the frontend for the create listing functionality

--- front-end-client/src/pages/CreateListing.jsx
+++ front-end-client/src/pages/CreateListing.jsx
@@ -4,41 +4,80 @@ import { useSelector, useDispatch } from "react-redux";
 const CreateListing = () => {
 
   return (
-    <main className="p-3 max-w-4xl mx-auto">
-      <h1 className="text-3xl font-semibold my-3 text-center">
+    <main className="p-3 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-semibold my-7 text-center">
         Create Listing
       </h1>
-      <form className="flex flex-col sm:flex-row">
+      <form className="flex flex-col sm:flex-row gap-4">
         <div className="flex gap-4 flex-col flex-1">
-          <input type="text" placeholder="name" name="name" className="border p-3 rounded-lg" id="name" maxLength="62" minLength="5" required/>
+          <input type="text" placeholder="Name" name="name" className="border p-3 rounded-lg" id="name" maxLength="62" minLength="5" required/>
           <textarea type="text" placeholder="Description" name="description" id="description" className="border p-3 rounded-lg" required />
-          <input type="text" placeholder="address" name="address" id="address" className="border p-3 rounded-lg" required/>
+          <input type="text" placeholder="Address" name="address" id="address" className="border p-3 rounded-lg" required/>
 
-          <div className="flex gap-5">
+          <div className="flex gap-5 flex-wrap">
             <div className="flex gap-2">
             
-              <input type="checkbox" id="sell" name="sell" className="w-5"/>
+              <input type="checkbox" id="sell" name="sell" className="w-4"/>
               <span>Sell</span>
 
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="rent" name="rent" className="w-5"/>
+              <input type="checkbox" id="rent" name="rent" className="w-4"/>
               <span>Rent</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="parking" name="parking" className="w-5"/>
+              <input type="checkbox" id="parking" name="parking" className="w-4"/>
               <span>Parking Spot</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="furnished" name="furnished" className="w-5"/>
+              <input type="checkbox" id="furnished" name="furnished" className="w-4"/>
               <span>Furnished</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="offer" name="offer" className="w-5"/>
+              <input type="checkbox" id="offer" name="offer" className="w-4"/>
               <span>Offer</span>
             </div>
           </div>
+          <div className="flex gap-6 flex-wrap">
+            <div className="flex gap-2 items-center">
+              <input type="number" className="outline-none p-3 rounded-lg text-sm border border-gray-300 border-b-2 border-t-0 border-r-0 border-l-0" max= '10' min= '1' id="bedrooms" name="bedrooms" required/>
+              <p>Beds</p>
+            </div>
+            <div className="flex gap-2 items-center">
+              <input type="number" className="outline-none p-3 rounded-lg text-sm border border-gray-300 border-b-2 border-t-0 border-r-0 border-l-0" id="bathrooms" name="bathrooms" min={1} max={10} required/>
+              <p>Bath</p>
+            </div>
+
+            <div className="flex gap-2 items-center">
+              <input type="number" className="outline-none p-3 rounded-lg text-sm border border-gray-300 border-b-2 border-t-0 border-r-0 border-l-0" id="regularprice" name="regularPrice" required min={50} max={10000000}/>
+              <div className="flex flex-col">
+                <p>Regular Price</p>
+                <p className="text-xs text-center">(&#36; / Month)</p>
+              </div>
+
+            </div>
+
+            <div className="flex gap-2 items-center">
+              <input type="number" className="outline-none p-3 rounded-lg text-sm border border-gray-300 border-b-2 border-t-0 border-r-0 border-l-0" id="discprice" name="discountPrice" required min={50} max={10000000}/>
+              <div className="flex flex-col items-center">
+                <p>Discounted Price</p>
+                <span className="text-xs text-center">(&#36; / Month)</span>
+              </div>
+            </div>
+
+          </div>
         </div>
+        <div className="flex flex-col flex-1 gap-4">
+          <p className="font-semibold">Images:
+            <span className="font-normal text-gray-600 ml-2">The first image will be the cover (max 6)</span>
+          </p>
+          <div className="flex gap-4">
+            <input className="p-3 border border-gray-300 rounded w-full" type="file" id="images" accept="image/*" multiple />
+            <button className="p-3 bg-green-700 text-white hover:bg-white transition hover:text-green-700 border hover:border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">Upload</button>
+          </div>
+          <button className="bg-slate-700 p-3 rounded-lg text-white uppercase hover:bg-white hover:text-slate-700 border hover:border-slate-700 disabled:opacity-80">Create Listing</button>  
+        </div>
+        
       </form>
     </main>
   )
