03a4874f2e8a550bb2420c1522eeaa4467506d78
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-09-06 23:05:46 -0500

    Created the backend API to get all the listings of a user

--- back-end-api/controllers/listings.controller.js
+++ back-end-api/controllers/listings.controller.js
@@ -19,3 +19,22 @@ export const CreateListingController = async (req, res, next) => {
 
 
 }
+
+export const GetListingsController = async (req, res, next) => {
+
+  const {id} = req.params;
+  
+  console.log(req.params);
+  console.log(id);
+
+  if(id !== req.user.id) return next(errorHandler(401, "You are not authorized to fetch the listings"));
+
+  try{
+    const listings = await Listing.find({createdBy:id});
+    return res.status(200).json({success: true, results:listings.length, data:listings});
+  }
+  catch(err){
+    return next(err);
+  }
+
+}
