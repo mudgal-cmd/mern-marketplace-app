0fb7ceaac6a826fa9e9bf21177bfb6ab6ccbbefb
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-09-10 21:49:40 -0500

    Designed the PDP Listing page

--- front-end-client/src/pages/Listing.jsx
+++ front-end-client/src/pages/Listing.jsx
@@ -4,6 +4,16 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
 import { Navigation } from "swiper/modules";
 import "swiper/css/bundle";
+import {
+  FaBath,
+  FaBed,
+  FaChair,
+  FaMapMarkedAlt,
+  FaMapMarkerAlt,
+  FaParking,
+  FaShare
+
+} from "react-icons/fa";
 
 const Listing = () => {
 
@@ -16,6 +26,8 @@ const Listing = () => {
   const [loadingPage, setLoadingPage] = useState(true);
 
   const [error, setError] = useState(false);
+
+  const [copied, setCopied] = useState(false);
 
   useEffect(() => {
     (async function(){
@@ -57,6 +69,60 @@ const Listing = () => {
             )
           )}
         </Swiper>
+        <div className="fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer">
+          <FaShare className="text-slate-500"
+          onClick={() => {
+            navigator.clipboard.writeText(window.location.href);
+            setCopied(true);
+            setTimeout(()=>{setCopied(false)}, 2000);
+          }}/>
+        </div>
+        {copied && (
+          <p className="fixed top-[22%] right-[1%] z-10 rounded-md bg-slate-100 p-2">Link Copied!</p>
+        )}
+
+        <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4">
+          <p className="text-2xl font-semibold">
+            {listingData.name} - ${" "} 
+            {listingData.offer ? listingData.discountPrice.toLocaleString("en-US")
+            : listingData.regularPrice.toLocaleString("en-US")}
+            {listingData.listingType === "rent" && " / month"}
+          </p>
+          <p className="flex items-center mt-6 gap-2 text-slate-600 text-sm">
+              <FaMapMarkerAlt className="text-green-700" />
+              {listingData.address}
+          </p>
+          <div className="flex gap-4">
+              <p className="bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+                {listingData.listingType === "rent"? "For Rent" : "For Sale"}
+              </p>
+              {listingData.offer && (<p className="bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md"> ${+listingData.regularPrice - +listingData.discountPrice} discount</p>)}
+          </div>
+            <p className="text-slate-800">
+              <span className="font-semibold text-black">
+                Description&nbsp;-&nbsp; 
+              </span>
+              {listingData.description}
+            </p>
+            <ul className="flex flex-wrap gap-6 sm:gap-8 text-green-900 font-semibold text-sm">
+              <li className="flex items-center gap-2 whitespace-nowrap">
+                <FaBed className="text-lg"/>
+                {listingData.bedrooms>1 ? `${listingData.bedrooms} beds`: `${listingData.bedrooms} bed`}
+              </li>
+              <li className="flex items-center gap-2 whitespace-nowrap">
+                <FaBath className="text-lg "/>
+                {listingData.bathrooms>1 ? `${listingData.bathrooms} baths`: `${listingData.bathrooms} bath`}
+              </li>
+              <li className="flex items-center gap-2 whitespace-nowrap">
+                <FaParking className="text-lg"/>
+                {listingData.parking ? "Parking Spot": "No Parking"}
+              </li>
+              <li className="flex items-center gap-2 whitespace-nowrap">
+                <FaChair className="text-lg"/>
+                {listingData.furnished ? "Furnished": "Unfurnished"}
+              </li>
+            </ul>
+        </div>
       </div>
     )}
   </main>
--- front-end-client/src/pages/Profile.jsx
+++ front-end-client/src/pages/Profile.jsx
@@ -235,10 +235,10 @@ function Profile(){
           <h1 className="text-2xl font-semibold text-center mt-7">Your Listings</h1>
           {userListings.map((listing) => (
           <div key={listing._id} className="border flex items-center justify-between rounded-lg p-3 gap-4">
-            <Link to={`listings/${listing._id}`}>
+            <Link to={`listing/${listing._id}`}>
               <img src={listing.imageURLs[0]} alt="listing cover" className="h-16 w-16 object-contain"/>
             </Link>
-            <Link className="text-slate-700 font-semibold flex-1 hover:underline truncate" to={`/listings/${listing._id}`}>
+            <Link className="text-slate-700 font-semibold flex-1 hover:underline truncate" to={`/listing/${listing._id}`}>
               <p>{listing.name}</p>
             </Link>
             <div className="flex flex-col">
