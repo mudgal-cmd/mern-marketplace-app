0fe804a51ba703163d7a022245fcfc37fb31db47
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-10-19 14:40:52 -0500

    Created the URL query param string and handled filters form submit

--- front-end-client/src/pages/Search.jsx
+++ front-end-client/src/pages/Search.jsx
@@ -2,49 +2,91 @@ import { ShowListing } from "../components/ShowListing";
 
 import {useState} from "react";
 
+import { useNavigate } from "react-router-dom";
+
 const SearchComponent = () => {
+
+  const navigate = useNavigate();
 
   const [listings, setListings] = useState({});
 
   const [searchFilterData, setSearchFilterData] = useState({
     searchterm: "",
-    type: "all",
+    listingType: "all",
     parking: false,
     furnished: false,
     offer: false,
-    sort: "createdAt",
-    sort_order: "desc"
+    sortBy: "createdAt",
+    sortOrder: "desc"
 
   }); // state to manage the search filters from the sidebar
 
   const handleSearchFilterOnChange = (event) => {
+    
+    if(event.target.name === "searchterm") {
 
+      setSearchFilterData({...searchFilterData, "searchterm": event.target.value});
+    };
+
+    if( event.target.name === "offer" || event.target.name === "parking" || event.target.name === "furnished"){
+
+      setSearchFilterData({...searchFilterData, [event.target.name]: event.target.checked || event.target.checked === "true" ? true: false});
+      console.log(event);
+    }
+    if(event.target.name === "all" || event.target.name === "rent" || event.target.name === "sell"){
+
+      setSearchFilterData({...searchFilterData, "listingType": event.target.name});
+    }
+    if( event.target.name === "sortBy"){
+      const sortBy = event.target.value.split("_")[0];
+      const sortOrder = event.target.value.split("_")[1];
+      setSearchFilterData({...searchFilterData, sortBy, sortOrder});
+    }
+  }
+
+  console.log(searchFilterData);
+
+
+  const handleSearchFilterSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams();
+    urlParams.set("searchterm", searchFilterData.searchterm);
+    urlParams.set("listingType", searchFilterData.listingType);
+    urlParams.set("parking", searchFilterData.parking? true: false);
+    urlParams.set("furnished", searchFilterData.furnished? true: false);
+    urlParams.set("offer", searchFilterData.offer);
+    urlParams.set("sortBy", searchFilterData.sortBy);
+    urlParams.set("sortOrder", searchFilterData.sortOrder);
+
+    const urlSearchQuery = urlParams.toString(); //converted the urlParams object into the query paramter string 
+    console.log(urlSearchQuery);
+    navigate(`/search?${urlSearchQuery}`); // navigating the user to the page
   }
 
   return (
     <div className="flex flex-col md:flex-row">
       <div className="p-7 border-b-2 md:border-r-2 md:min-h-screen"> {/* for the left side or the search part/filters */}
-        <form className="flex flex-col gap-8">
+        <form className="flex flex-col gap-8" onSubmit={handleSearchFilterSubmit}>
           <div className="flex items-center gap-4">
             <label className="whitespace-nowrap font-semibold">Search Term:</label>
-            <input type="text" id="searchterm" name="searchterm" placeholder="Start searching here..." className="p-3 rounded-lg border w-full"/>
+            <input type="text" id="searchterm" name="searchterm" placeholder="Start searching here..." className="p-3 rounded-lg border w-full" onChange={handleSearchFilterOnChange} value={searchFilterData.searchterm}/>
           </div>
           <div className="flex gap-3 flex-wrap items-center">
             <label className="font-semibold">Type:</label>
             <div className="flex gap-2">
-              <input type="checkbox" id="all" className="w-4"/>
+              <input type="checkbox" id="all" name="all" checked={searchFilterData.listingType === "all"} className="w-4" onChange={handleSearchFilterOnChange}/>
               <span>Rent & sale</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="rent" name="listingtype" className="w-4"/>
+              <input type="checkbox" id="rent" name="rent" className="w-4" onChange={handleSearchFilterOnChange} checked = {searchFilterData.listingType === "rent"}/>
               <span>Rent</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="sell" name="listingtype" className="w-4"/>
+              <input type="checkbox" id="sell" name="sell" checked = {searchFilterData.listingType === "sell"} className="w-4" onChange={handleSearchFilterOnChange}/>
               <span>Sale</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="offer" className="w-4"/>
+              <input type="checkbox" id="offer" name="offer" className="w-4" value={searchFilterData.offer === true} onChange={handleSearchFilterOnChange}/>
               <span>Offer</span>
             </div>
           </div>
@@ -52,22 +94,22 @@ const SearchComponent = () => {
           <div className="flex gap-3 flex-wrap items-center">
             <label className="font-semibold">Amenities:</label>
             <div className="flex gap-2">
-              <input type="checkbox" id="parking" name="parking" className="w-4"/>
+              <input type="checkbox" id="parking" name="parking" checked = {searchFilterData.parking === true} className="w-4" onChange={handleSearchFilterOnChange}/>
               <span>Parking</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="furnished" name="listingtype" className="w-4"/>
+              <input type="checkbox" id="furnished" name="furnished" checked = {searchFilterData.furnished === true} className="w-4" onChange={handleSearchFilterOnChange}/>
               <span>Furnished</span>
             </div>
           </div>
 
           <div className="flex items-center gap-3">
             <label className="font-semibold">Sort:</label>
-            <select className="p-3 border-none rounded-lg outline-none" id="sort_order" >
-              <option value="Latest">Price low to high</option>
-              <option value="Latest">Price high to low</option>
-              <option value="Latest" selected= "selected">Latest</option>
-              <option value="Latest">Oldest</option>
+            <select className="p-3 border-none rounded-lg outline-none" id="sortBy" name="sortBy" defaultValue={searchFilterData.sortOrder }onChange={handleSearchFilterOnChange}>
+              <option value="regularPrice_desc">Price low to high</option>
+              <option value="regularPrice_asc">Price high to low</option>
+              <option value="createdAt_desc">Latest</option>
+              <option value="createdAt_asc">Oldest</option>
             </select>
           </div>
 
