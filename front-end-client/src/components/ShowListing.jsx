073f3a021d115e56e1406e5515c4acc9e870e353
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-10-22 20:28:37 -0500

    MERN-app

--- front-end-client/src/components/ShowListing.jsx
+++ front-end-client/src/components/ShowListing.jsx
@@ -5,7 +5,7 @@ import {MdLocationOn} from "react-icons/md";
 
 export const ShowListing = ({listing}) => {
   return (
-    <div className=" shadow-md bg-white h-80 rounded-xl mt-5 ml-5 gap-4 hover:shadow-lg transition-shadow duration-300 w-full sm:w-[300px] flex flex-col overflow-hidden">
+    <div className=" shadow-md bg-white rounded-xl mt-5 ml-5 gap-4 hover:shadow-lg transition-shadow duration-300 w-full sm:w-[300px] h-full flex flex-col overflow-hidden">
       <Link to={`/listing/${listing._id}`}>
         <img src={`${listing.imageURLs[0]}`} alt="Listing Cover" className="h-[320px] sm:h-[200px] w-full object-cover hover:scale-105 transition-scale duration-300 rounded-xl"/>
         <div className="p-3 flex flex-col gap-2 w-full">
@@ -16,9 +16,11 @@ export const ShowListing = ({listing}) => {
             <p className="truncate text-sm text-gray-600 w-full">{listing.address}</p>
           </div>
           <p className="text-sm text-gray-600 line-clamp-2">{listing.description}</p>
+          <p>${listing.offer? listing.discountPrice : listing.regularPrice}</p>
+          {/* {listing.offer? <p>{listing.discountPrice}</p> : <p>{listing.regularPrice}</p>} */}
         </div>  
       </Link>
-      <p>{listing.listingType}</p>
+      {/* <p>{listing.listingType}</p> */}
     </div>
   );
 }
