3dd0c252012c71d4e7ca067b8581489763c9d96a
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-11-07 19:37:12 -0600

    Added debug statements for user not getting signed out

--- back-end-api/utils/verifyUser.js
+++ back-end-api/utils/verifyUser.js
@@ -4,11 +4,11 @@ import { errorHandler } from "./error.js";
 export const verifyUserToken = (req, _, next) => { // function to verify the user jwt token saved in the cookie.
   // console.log("User verified");
   // console.log("Verify token executed");
-  console.log(req.cookies);
+  console.log(`Cookies: ${req.cookies}`);
   console.log(req);
   const {access_token} = req.cookies; //destructuring the cookie to get the current token.
 
-  // console.log(req); 
+  
 
   // console.log(access_token);
 
@@ -16,7 +16,7 @@ export const verifyUserToken = (req, _, next) => { // function to verify the use
 
   jwt.verify(access_token, process.env.JWT_SECRET, (err, decoded) => { //jwt.verify() method takes 3 arguments, the callback being the 3rd one.
     if(err) return next(errorHandler(403, err)); // if there is any error during the token verification, break the execution and send the error
-
+    console.log(decoded);
     req.user = decoded; // saving the decoded data which looks like - { id: '665279ee49172e2f2d179a5f', iat: 1722643603 }
     // console.log(decoded.id);
     next(); // passing the execution to the next MW in the line, i.e., updateUserController.
